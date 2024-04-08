bcf2240ec097743fde886052179b84c4d5fe2bd3
Willian Lira <willian_llyra@hotmail.com>
2024-04-08 03:09:19 -0400

    Add infra

---
+++ Thunders.Todo.Infra/Context/TodoDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Thunders.Todo.Domain.Entities;
+
+namespace Thunders.Todo.Infra.Context;
+
+public class TodoDbContext : DbContext
+{
+    public DbSet<TodoTask> TodoTasks { get; set; }
+
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options)
+    {
+        if (Database.EnsureCreated())
+        {
+            SeedData();
+        }
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .LogTo(Console.WriteLine)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    private void SeedData()
+    {
+        TodoTasks.Add(TodoTask.Create("Fazer compras", "Comprar leite, ovos e pão"));
+        TodoTasks.Add(TodoTask.Create("Estudar para a prova", "Revisar os conceitos principais"));
+
+        SaveChanges();
+    }
+}
